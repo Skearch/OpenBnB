953278f6c596af370a6a620477b8e18c0f3ed39c
Skearch <127990061+Skearch@users.noreply.github.com>
2025-05-19 17:44:45 +0800

    Update

--- src/routes/bookingRoutes.js
+++ src/routes/bookingRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { list } = require("../controllers/bookingController");
 
 router.get("/list/:propertyId", (req, res) => {
   res.json({ bookings: [] });
